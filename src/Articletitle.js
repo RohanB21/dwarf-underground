ad05b41df01f4b89f904e8a451fca661baa708a5
Rohan <rbalel@purdue.edu>
2017-05-23 10:26:33 -0400

    Split into 6 components

---
+++ src/Articletitle.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './Articletitle.css';
+
+class Articletitle extends Component {
+    render() {
+        return (
+            <h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
+        )
+    }
+}
+export default Articletitle;
